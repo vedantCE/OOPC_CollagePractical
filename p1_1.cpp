27455b8933add983700a88f90faf38287c89c8d5
VEDANT BHATT <vedantbhattce28@gmail.com>
2025-03-12 22:31:07 +0530

    Add documnetation

--- p1_1.cpp
+++ p1_1.cpp
@@ -1,3 +1,12 @@
+/*
+==============================================
+    PROGRAM: Simple Bank Account Management System
+    AIM: To perform basic banking operations like deposit, withdraw, and check balance.
+    DEVELOPER: Vedant Bhatt
+    LAST UPDATED: March 12, 2025
+==============================================
+*/
+
 #include <iostream>
 using namespace std;
 
@@ -5,85 +14,88 @@ using namespace std;
 class BankAccount
 {
 public:
-    string HolderName;
-    int accountNumber,balance;
+    string holder_name;
+    int account_number, balance;
 
     // Deposit money function
     void deposit(int amount)
     {
-        if(amount>0)
+        if (amount > 0)
         {
-            balance+=amount;
+            balance += amount;
         }
     }
 
     // Withdraw money function
     void withdraw(int amount)
     {
-        if(amount>0 && amount<balance)
+        if (amount > 0 && amount < balance)
         {
-            balance-=amount;
+            balance -= amount;
         }
-        else if(amount>balance)
+        else if (amount > balance)
         {
-            cout<<"Not enough balance!"<<endl;
+            cout << "Not enough balance!" << endl;
         }
     }
 
-    // Diplay Current bankbalance
+    // Display current bank balance
     void display_balance()
     {
-        cout<<"Current Balance: "<<balance;
+        cout << "Current Balance: " << balance;
     }
 };
-
 
 // Main Function
 int main()
 {
-
-    // Declaring First bank accunt
-    BankAccount B1;
-    B1.balance=10000;
+    // Declaring first bank account
+    BankAccount b1;
+    b1.balance = 10000;
     int amount;
     int choice;
 
-    // Taking bankaccount detail
-    cout<<"Enter your name: ";
-    cin>>B1.HolderName;
-    cout<<"Enter your account number: ";
-    cin>>B1.accountNumber;
+    // Taking bank account details
+    cout << "Enter your name: ";
+    cin >> b1.holder_name;
+    cout << "Enter your account number: ";
+    cin >> b1.account_number;
 
-    cout<<"Enter 1 For DEPOSIT"<<endl<<"Enter 2 For WITHDRAW Enter 2"<<endl<<"Enter 3 For CHECKING CURRENT BALANCE "<<endl<<"Enter 4For EXIT "<<endl;
-    do{
-        cout<<endl;
-        cout<<"Enter your choice: ";
-        cin>>choice;
+    cout << "Enter 1 For DEPOSIT" << endl
+         << "Enter 2 For WITHDRAW" << endl
+         << "Enter 3 For CHECKING CURRENT BALANCE" << endl
+         << "Enter 4 For EXIT" << endl;
 
-        switch(choice)
+    do
+    {
+        cout << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice)
         {
         case 1:
-            cout<<"Enter amount for Deposit: ";
-            cin>>amount;
-            B1.deposit(amount);
+            cout << "Enter amount for Deposit: ";
+            cin >> amount;
+            b1.deposit(amount);
             break;
         case 2:
-            cout<<"Enter amount for Withdraw: ";
-            cin>>amount;
-            B1.withdraw(amount);
+            cout << "Enter amount for Withdraw: ";
+            cin >> amount;
+            b1.withdraw(amount);
             break;
         case 3:
-            B1.display_balance();
+            b1.display_balance();
             break;
         case 4:
-            cout<<"\nThank you!"<<endl;
+            cout << "\nThank you!" << endl;
             break;
         default:
-            cout<<"Enter valid choice!"<<endl;
+            cout << "Enter valid choice!" << endl;
         }
-    }
-    while(choice!=4);
+    } while (choice != 4);
 
-    cout<<"\nName: Vedant Bhatt"<<endl<<"ID: 24CE013";
+    cout << "\nName: Vedant Bhatt" << endl
+         << "ID: 24CE013";
     return 0;
 }
